fe759ad7d2616ab20bfcc38c8c2ad5d6e93e98e5
Arthur Ribeker <dogcattrendsofc@gmail.com>
2025-10-22 03:25:33 -0300

    commit feito

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,11 @@ export default defineConfig({
     include: ['tests/**/*.test.{ts,tsx}'],
     passWithNoTests: true,
     setupFiles: ['tests/setup/test-env.ts'],
-  coverage: { reporter: ['text','html','json-summary'], enabled: true, reportsDirectory: 'coverage' }
+    coverage: { reporter: ['text','html','json-summary'], enabled: true, reportsDirectory: 'coverage' },
+    env: {
+      NEXT_PUBLIC_SUPABASE_URL: 'https://npmnuihgydadihktglrd.supabase.co',
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wbW51aWhneWRhZGloa3RnbHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2OTAwODYsImV4cCI6MjA3MTI2NjA4Nn0.fKsON7BroWxgzxVaF0V4Jh3AZdKlW0a70J29fGvTjGg'
+    }
   },
   resolve: {
     alias: {
